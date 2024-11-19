f7e043854269b0615c19d134bd6f61368b1c0857
NoCr1mePls <123958344+NoCr1mePls@users.noreply.github.com>
2024-11-19 11:57:39 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,19 @@ using Avans.StatisticalRobot;
 
 Console.WriteLine("Hello world");
 
-while(true) 
-{
-   
+Led led = new Led(5);
+Button button = new Button(6);
+
+while (true)
+{   
+    led.SetOn();
+    Robot.Wait(100);
+    led.SetOff();
+    Robot.Wait(100);
+    if(button.GetState().Equals("Pressed")){
+        Robot.Motors(100,100);
+    }
+    else{
+        Robot.Motors(0,0);
+    }
 }
